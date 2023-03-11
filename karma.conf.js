c0906af642ef59e1ef2e35977d541ae06d14def8
RafalJ7 <259384@student.pwr.edu.pl>
2023-03-11 11:08:00 +0100

    jasmine full

--- karma.conf.js
+++ karma.conf.js
@@ -1,13 +1,47 @@
-module.exports = function(config) {
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
   config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-jasmine-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
     client: {
       jasmine: {
-        random: true,
-        seed: '4321',
-        oneFailurePerSpec: true,
-        failFast: true,
-        timeoutInterval: 1000
-      }
-    }
-  })
-}
+        // you can add configuration options for Jasmine here
+        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+        // for example, you can disable the random execution with `random: false`
+        // or set a specific seed with `seed: 4321`
+      },
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    jasmineHtmlReporter: {
+      suppressAll: true // removes the duplicated traces
+    },
+    coverageReporter: {
+      dir: require('path').join(__dirname, './coverage/my-first-project'),
+      subdir: '.',
+      reporters: [
+        { type: 'html' },
+        { type: 'text-summary' }
+      ]
+    },
+    reporters: ['jasmine'],
+    logLevel: config.LOG_ERROR,
+    autoWatch: false,
+    singleRun: true,
+    browserNoActivityTimeout: 40000,
+    browsers: ['ChromeHeadlessCI'],
+    customLaunchers: {
+    ChromeHeadlessCI: {
+    base: 'ChromeHeadless',
+    flags: ['--no-sandbox']
+  }
+},
+    restartOnFileChange: true
+  });
+};
